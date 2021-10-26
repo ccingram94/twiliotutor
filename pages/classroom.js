6a3ed39358134e167ca7f3273ccbbd8c258ae164
Constancegit status <constanceingram94@gmail.com>
2021-10-26 13:08:25 -0500

    removing old twilio stuff to reimplement

--- pages/classroom.js
+++ pages/classroom.js
@@ -7,36 +7,6 @@ import Footer from '../components/footer'
 import Auth from '../components/auth'
 import { TwilioError } from 'twilio-video'
 
-const tryButton = document.getElementById("try-video-button");
-const stopButton = document.getElementById("stop-video-button");
-const container = document.getElementById("video-div")
-
-
-async function startVideo() {
-  startVideoUI();
-  const track = await TwilioError.Video.createLocalVideoTrack();
-  container.append(track.attach());
-  stopButton.addEventListener("click", function() {
-    stopVideo(track);
-  });
-}
-
-function stopVideo(track) {
-  track.stop();
-  endVideoUI();
-}
-
-function startVideoUI() {
-  tryButton.disabled = true;
-  stopButton.disabled = false;
-  container.innerHTML = "";
-}
-
-function endVideoUI() {
-  tryButton.disabled = false;
-  stopButton.disabled = true;
-  container.innerHTML = "";
-}
 
 
 const Classroom = () => {
