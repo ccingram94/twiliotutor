bfd3925f8237918ae166fa9a5949a83d8e8b6f48
Constancegit status <constanceingram94@gmail.com>
2021-10-26 13:16:57 -0500

    added variable loggedin and loggedout views to profile page

--- pages/profile.js
+++ pages/profile.js
@@ -4,11 +4,13 @@ import { signIn, signOut, useSession } from 'next-auth/react'
 import AuthTest from '../components/authtest'
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
+import AcademicCap from '@heroicons/react/outline/AcademicCapIcon'
 
 const Home = () => {
   const { data: session, status } = useSession();
-  return (
-    <div className="">
+  if (status === "authenticated") {
+    return (
+      <div className="">
       <Head>
         <title>twilio tutor</title>
         <meta name="description" content="tutor with twilio" />
@@ -31,6 +33,36 @@ const Home = () => {
       </main>
       <Footer />
     </div>
+    )
+  }
+  return (
+    <div className="">
+    <Head>
+      <title>twilio tutor</title>
+      <meta name="description" content="tutor with twilio" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Navbar/>
+    <main className="heropattern mx-auto w-full min-h-screen">
+      <div className="flex flex-wrap mx-auto items-center justify-center pt-32">
+        <div className="container mx-auto items-center justify-center bg-indigo-200 rounded-xl px-12 py-12">
+          <h1 className="text-indigo-900 font-bold text-center text-3xl">hmmm... you don't seem to be logged in</h1>
+        </div>
+      </div>
+      <div className="flex flex-wrap mx-auto items-center justify-center pt-6">
+        <div className="container mx-auto items-center justify-center bg-indigo-200 rounded-xl px-12 py-12">
+          <h1 className="text-indigo-900 font-bold text-center text-3xl">you can log in here!</h1>
+          <div className="bg-transparent hover:bg-blue-900 ease-linear transition-all border border-blue-300 hover:border-transparent py-2 px-2 rounded">
+          <button onClick={signIn} className="uppercase text-blue-300 hover:text-white text-center justify-center items-center text-3xl">
+            <h1> sign in </h1>
+            <AcademicCap className="h-6 w-6" />
+          </button>
+      </div>
+        </div>
+      </div>
+    </main>
+    <Footer />
+  </div>
   )
 }
 
