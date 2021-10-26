61bfe8cf092eb2ebdebf01061926a2507cfa364f
Constancegit status <constanceingram94@gmail.com>
2021-10-26 13:23:05 -0500

    fixing mobile layout

--- components/navbar.js
+++ components/navbar.js
@@ -12,7 +12,7 @@ export default function Navbar(props) {
     <>
       <nav className="top-0 fixed z-50 w-full flex flex-row items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow">
           <div><Link href="/"><h2 className="uppercase font-bold justify-start text-blue-300 hover:text-blue-500 ease-linear transition-all text-xl">twilio tutor</h2></Link></div>
-          <div className="flex flex-row flex-wrap px-4 text-xs space-x-4">
+          <div className="flex flex-row flex-wrap px-4 text-xs space-x-4 justify-center items-center text-center">
             <AuthButton/>
             <ClassroomButton />
           </div>
